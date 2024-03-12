e078515d9861262845e9a6d68a99f3819f695e2b
Nadu_Dev <inbox.nadun@gmail.com>
2024-03-12 10:57:09 +0530

    temp commit

--- ServiceSubscriber/src/com/mtit/UIconsumer/ServiceActivator.java
+++ ServiceSubscriber/src/com/mtit/UIconsumer/ServiceActivator.java
@@ -2,7 +2,6 @@ package com.mtit.UIconsumer;
 
 import org.osgi.framework.BundleActivator;
 
-
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import usermanagementproducer.UserManagePublish;
@@ -10,38 +9,40 @@ import com.mtit.UIproducer.ServicePublish;
 
 public class ServiceActivator implements BundleActivator {
 
-	//ServiceReffernce
-	ServiceReference  UMserviceReference;
-	ServiceReference  serviceReference;
-
+	// ServiceReffernce
+	ServiceReference UMserviceReference;
+	ServiceReference serviceReference;
 
 	public void start(BundleContext context) throws Exception {
-	
-		System.out.println("................UI Consumer is started................");
-		//ui producer service
-		serviceReference = context.getServiceReference(ServicePublish.class.getName());
-		ServicePublish servicePublish = (ServicePublish) context.getService(serviceReference);
-		
-		//user management producer service
-		UMserviceReference = context.getServiceReference(UserManagePublish.class.getName());
-		UserManagePublish UMproducerService = (UserManagePublish) context.getService(UMserviceReference);
-		
-		
 
-		//initiates the service with conncection to the UI
-		UMproducerService.addLoginLogic();
-		UMproducerService.addRegistrationLogic();
-		servicePublish.initiate(context);
-		servicePublish.headStart();
-		
+		System.out.println(
+				"................UI Consumer is started................");
+		// ui producer service
+		try {
+			serviceReference = context.getServiceReference(ServicePublish.class.getName());
+			ServicePublish servicePublish = (ServicePublish) context.getService(serviceReference);
+
+			// user management producer service
+			UMserviceReference = context.getServiceReference(UserManagePublish.class.getName());
+			UserManagePublish UMproducerService = (UserManagePublish) context.getService(UMserviceReference);
+			// initiates the service with conncection to the UI
+			UMproducerService.addLoginLogic();
+			UMproducerService.addRegistrationLogic();
+			servicePublish.initiate(context);
+			servicePublish.headStart();
+			
+		} catch (NullPointerException e) {
+			System.out.println(
+					"UI Consumer Looks like Referenced Service is not started yet. Please start the service(UI producer, UM Service) first.");
+		}
+
 	}
-	
 
 	public void stop(BundleContext context) throws Exception {
-		
-		
-		//detach the service
-		System.out.println("................UI Consumer is stopped................");
+
+		// detach the service
+		System.out.println(
+				"................UI Consumer is stopped................");
 		context.ungetService(serviceReference);
 	}
 
